a12c73b5679c9abcbd4a78d12b000022dbe2a93e
Siddharth Choudhari <choudharishiddharth@gmail.com>
2023-04-01 19:47:31 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,136 @@
+import { useState, useEffect } from "react";
+
+import ArrowRightIcon from "@mui/icons-material/ArrowRight";
+import ArrowLeftIcon from "@mui/icons-material/ArrowLeft";
+import PauseCircleIcon from "@mui/icons-material/PauseCircle";
+import PlayCircleIcon from "@mui/icons-material/PlayCircle";
+
+import images from "./data/images";
+
+export const App = () => {
+  const [index, setIndex] = useState(0);
+  const [autoplay, setAutoplay] = useState(false);
+  const [darkMode, setDarkMode] = useState(
+    JSON.parse(localStorage.getItem("darkMode") || false)
+  );
+
+  useEffect(() => {
+    localStorage.setItem("darkMode", JSON.stringify(darkMode));
+
+    if (darkMode) {
+      document.body.style.backgroundColor = "#181818";
+    } else {
+      document.body.style.backgroundColor = "#fff";
+    }
+  }, [darkMode]);
+
+  const handleNext = () => {
+    setIndex((prevIndex) => (prevIndex + 1) % images.length);
+  };
+
+  const handlePrev = () => {
+    setIndex((prevIndex) => (prevIndex + images.length - 1) % images.length);
+  };
+
+  const handleThumbnailClick = (index) => {
+    setIndex(index);
+  };
+
+  useEffect(() => {
+    let intervalId;
+    if (autoplay) {
+      intervalId = setInterval(() => {
+        setIndex((prevIndex) => (prevIndex + 1) % images.length);
+      }, 3000);
+    }
+    return () => clearInterval(intervalId);
+  }, [autoplay]);
+
+  let currentImage = images[index];
+
+  return (
+    <>
+      <div className="darkMode-toggle">
+        <input
+          onClick={() => setDarkMode(!darkMode)}
+          type="checkbox"
+          className="checkbox"
+          id="checkbox"
+          checked={darkMode}
+        />
+        <label
+          for="checkbox"
+          className="label"
+          style={{ border: darkMode && "1px solid grey" }}
+        >
+          <i className="fas fa-sun"></i>
+          <i className="fas fa-moon"></i>
+          <div className="ball"></div>
+        </label>
+      </div>
+      <div
+        className="container"
+        style={{ backgroundColor: darkMode && "#121212" }}
+      >
+        <div className="image-slider">
+          <img src={currentImage.src} alt={currentImage.title} id="mainImage" />
+          <div className="controls">
+            <ArrowLeftIcon
+              onClick={handlePrev}
+              sx={{ fontSize: "80px", color: darkMode && "white" }}
+            />
+            <div className="thumbnails">
+              {images.map((image, i) => (
+                <img
+                  key={image.id}
+                  onClick={() => handleThumbnailClick(i)}
+                  src={image.src}
+                  alt={image.title}
+                  style={{ cursor: "pointer" }}
+                  className={`thumbnail ${currentImage === image && "active"}`}
+                />
+              ))}
+            </div>
+            <ArrowRightIcon
+              onClick={handleNext}
+              sx={{
+                fontSize: "80px",
+                cursor: "pointer",
+                color: darkMode && "white",
+              }}
+            />
+          </div>
+          <div className="autoplay">
+            {autoplay ? (
+              <PauseCircleIcon
+                style={{
+                  fontSize: "80px",
+                  cursor: "pointer",
+                  color: darkMode && "white",
+                }}
+                onClick={() => setAutoplay((prevAutoplay) => !prevAutoplay)}
+              />
+            ) : (
+              <PlayCircleIcon
+                style={{
+                  fontSize: "80px",
+                  cursor: "pointer",
+                  color: darkMode && "white",
+                }}
+                onClick={() => setAutoplay((prevAutoplay) => !prevAutoplay)}
+              />
+            )}
+          </div>
+        </div>
+        <div className="image-info">
+          <h1 id="title" style={{ color: darkMode && "#fff" }}>
+            {currentImage.title}
+          </h1>
+          <p id="description" style={{ color: darkMode && "#b3b3b3" }}>
+            {currentImage.description}
+          </p>
+        </div>
+      </div>
+    </>
+  );
+};
---
+++ src/data/images.js
@@ -0,0 +1,32 @@
+const images = [
+  {
+    id: 1,
+    src: "https://i.guim.co.uk/img/media/9d006cc5a204ae3a436c59f5dcc7dbfacc0e76be/0_0_5822_3863/master/5822.jpg?width=1300&quality=45&dpr=2&s=none",
+    title: "A guard treating an immigrant at the southern border, Arizona, US",
+    description:
+      "19 May There’s a stretch of the Arizona border with Mexico where crossing into the US is fairly straightforward, through a shallow river. I headed to the south-west of the state, after hearing reports of large numbers of people crossing. I’ve covered the border before, but never at this location. I positioned myself near a hole in the barrier. At dusk, a large group of Jamaican people came through rapidly. Most collapsed on US soil. Some prayed, others lay on their backs, consumed by relief and exhaustion. After a while, most of them would stand up, refreshed. But I noticed this one man leaning up against the fence; he was confused and drained, unable to move or communicate. Border agents radioed for a medical team. Fifteen minutes later, he was put on to an IV drip, which a border guard held for him.",
+  },
+  {
+    id: 2,
+    src: "https://i.guim.co.uk/img/media/e320dfe40f3acd79cff59c80f9f92e0ecf90a0a9/0_0_5464_3640/master/5464.jpg?width=1300&quality=45&dpr=2&s=none",
+    title: "A protest against Covid-19 mandates, Maryland, US",
+    description:
+      "5 March During the pandemic, I started documenting the anti-mandate protests. At first I was in Canada, where truckers blocked roads, before the action moved over to the US. For those of us who had watched the “stop the steal” protests closely, the January 6 storming of the Capitol wasn’t hugely surprising. I wanted to keep an eye on this new wave, in case it led to another major conflict. I followed it in Tennessee and New York, before I heard about a protest in Hagerstown, Maryland. It was being held in an abandoned rural fairground, but being so close to Washington DC, there was talk they might attempt to block roads into the capital. The atmosphere was actually pretty mild: it was more of a weekend-long rally – lots of Trump flags – than direct action. Instead, it was a general conservative get-together. It felt like a music festival, but with no shows. It was an expression of identity, as things often are, I think: people looking for some sort of community.",
+  },
+  {
+    id: 3,
+    src: "https://i.guim.co.uk/img/media/33f7852d2ee173066d9a3d311d424e3b19ced4ac/0_0_5874_3908/master/5874.jpg?width=1300&quality=45&dpr=2&s=none",
+    title: "Migrants move a smuggling boat into the water, Gravelines, France",
+    description:
+      "12 October For years, I've been covering migration in northern France, spending periods focused on different parts of the experience. I've spent time in camps documenting daily life, on board the boats saving lives at sea, and with the French authorities. This year, I spent more time waiting on beaches, hoping to photograph smuggling operations. In October, I was on the beach in Gravelines, near Dunkirk. A few days passed, and I saw nothing. Then, one morning at dawn, I found many people hoping to cross the Channel. They had travelled overnight on buses, and then hid for hours in the darkness near the beach waiting for their moment. At about 8am, this group carried a boat down to the water. It's sad for me, seeing their struggle.",
+  },
+  {
+    id: 4,
+    src: "https://i.guim.co.uk/img/media/2c82f4d73331363db692791382e4238b1a60e00a/0_0_3500_2334/master/3500.jpg?width=880&quality=45&dpr=2&s=none",
+    title: "A plane drops retardant over a wildfire, California, US",
+    description:
+      "7 September In other states, journalists have to stay behind police lines, but in California we’re allowed into the fires. Heading in always feels counterintuitive – you’re going directly towards danger. On my first day at this Fairview fire in Hemet, there had been two fatalities. By day two, there were more resources on hand. I found a spot on a hill, looking out at a neighbourhood at risk, watching smaller planes and helicopters paint a retardant line to stop the fire consuming the buildings. Then just over the burning ridge line, this huge tanker plane made its way past us. Science tells us what climate breakdown means. Seeing it up close, so close to home, is really alarming.",
+  },
+];
+
+export default images;
